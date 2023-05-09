7bcd406a1f70c54efc05875bddad995889e38065
lestradavaz <lestradavaz@outlook.com>
2023-05-09 12:41:52 -0500

    fixed some css

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,25 +9,25 @@ export const objects = {
     aboutUsCards: [
         {
             id: 1,
-            icon: 'info',
+            icon: 'handshake',
             title: 'Compromiso',
             desc: 'Estamos comprometidos a trabajar duro y esforzarnos por ser lo mejor que podemos ser. Nos comprometemos a dar nuestro máximo esfuerzo en cada entrenamiento y en cada juego.'
         },
         {
             id: 2,
-            icon: 'info',
+            icon: 'local_fire_department',
             title: 'Pasión',
             desc: 'Tenemos una gran pasión por el fútbol americano estamos comprometidos a hacer lo necesario para alcanzar nuestros objetivos y estamos enfocados a dar lo mejor de nosotros mismos en cada partido.'
         },
         {
             id: 3,
-            icon: 'info',
+            icon: 'groups_2',
             title: 'Trabajo en equipo',
             desc: 'Trabajamos juntos y apoyamos a nuestros compañeros de equipo en todo momento. Valoramos el trabajo en equipo y creemos que es fundamental para alcanzar el éxito.'
         },
         {
             id: 4,
-            icon: 'info',
+            icon: 'volunteer_activism',
             title: 'Respeto',
             desc: 'Mostramos respeto hacia nuestros oponentes, entrenadores, compañeros de equipo, árbitro y aficionados.'
         }
@@ -35,19 +35,19 @@ export const objects = {
     homeCards: [
         {
             id: 1,
-            icon: 'info',
+            icon: 'verified',
             title: 'Misión',
             desc: 'Nuestra misión es crear un ambiente donde los jugadores puedan desarrollar todo su potencial atlético y personal, mientras se esfuerzan por alcanzar el éxito deportivo y contribuyen a la comunidad a través del servicio y la responsabilidad social.'
         },
         {
             id: 2,
-            icon: 'info',
+            icon: 'visibility',
             title: 'Visión',
             desc: 'Ser un equipo de fútbol americano de élite que sea reconocido por su excelencia deportiva y su compromiso con la comunidad.'
         },
         {
             id: 3,
-            icon: 'info',
+            icon: 'diamond',
             title: 'Filosofía',
             desc: 'Fomentar la unión, el esfuerzo constante y la madurez para vivir las victorias y las derrotas dentro y fuera del campo de juego.'
         }
